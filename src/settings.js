39fd87901debab07419bae785fcda4ddb27588d9
honey <1730564896@qq.com>
2022-09-03 13:37:44 +0800

    人力资源初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'Vue Admin Template', // 项目标题
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, // 固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false // 固定左侧菜单logo
+}
